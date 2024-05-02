f5da25357a2df2e3c253ad30c6976c5d06f1e2aa
Matthew Hefferon <matthefferon@gmail.com>
2024-05-01 19:11:41 -0700

    Big commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-export default function Page() {
-  return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
-    </div>
-  );
-}
+import dynamic from "next/dynamic";
+
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
+
+export default App;
